a9b97eaeaac4ef5e4faa018d775de423b576fec7
jenniferchang <jenniferchang@localhost>
2022-09-01 12:19:41 -0500

    Done

--- src/studio0/LunchReceipt.java
+++ src/studio0/LunchReceipt.java
@@ -7,12 +7,12 @@ public class LunchReceipt {
 		double burritoPrice = 7.5;
 		double drinkPrice = 1.25;
 		double chipsPrice = 1.00;
-		double guacPrice = 2.50;
+		double guacPrice = 2.5;
 		
 		double subtotal = burritoPrice + drinkPrice + chipsPrice + guacPrice;
 		double taxRate = 0.04225;
-		double taxAmount = subtotal * taxRate;
-		double total = subtotal + taxAmount;
+		double taxAmount = subtotal * taxRate ;
+		double total = subtotal + taxAmount; 
 		
 		System.out.println("Your total was: $" + total);
 	}
